ae45a8ca8d4bcb03ab1a43bb44e827820de8e058
root <root@Built-here.localdomain>
2020-03-12 00:51:13 -0400

    experimenting with  flask_restfull

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
-from flask import Flask
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
 
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.environ.get('DB')
+api = Api(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, Aidly !'
 
-app.config['MONGO_URI'] = os.environ.get('DB')
-print(os.environ.get('DB'))
+
+class Opportunity(Resource):
+    def get(self,num):
+        return {
+            'result': num*2
+            }
+
+api.add_resource(Opportunity, '/opportunity/<int:num>')
+
 
 if(__name__ == "__main__"):
-    app.run(debug=True)
+    app.run(debug=True, port=1234)
