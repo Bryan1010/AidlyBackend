6b3c5b4138a500ea8274d7d97b1f50bf1c3500cc
Bryan Cruz <bryanc@fromuthtennis.com>
2020-06-14 02:18:14 -0400

    started to add token and password for users

--- models/users.py
+++ models/users.py
@@ -1,6 +1,10 @@
 from models import db
-from sqlalchemy import Column, Integer, String, DateTime, Text
+from mainapp import MAIN_APP
+from sqlalchemy import Column, Integer, String, DateTime, Text, Boolean
 from datetime import datetime
+from flask_bcrypt import generate_password_hash, check_password_hash
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
 
 class User(db.Model):
@@ -13,7 +17,8 @@ class User(db.Model):
     mission_statement = Column(Text)
     interests = db.relationship('UserInterest', backref='user')
 
-    password = Column(Text)
+    password = Column(String(255))
+    authenticated = Column(Boolean, default=False)
 
     company = db.relationship('CompanyUsers', backref='user')
 
@@ -23,6 +28,28 @@ class User(db.Model):
 
     def get_full_name(self):
         return f'{self.first_name} {self.last_name}'
+
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(MAIN_APP.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(MAIN_APP.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None  # valid token, but expired
+        except BadSignature:
+            return None  # invalid token
+        user = User.query.get(data['id'])
+        return user
 
 
 class Interest(db.Model):
--- routes/user.py
+++ routes/user.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, Response, request, json, jsonify, make_response
 from models.users import User, UserInterest
 # from models.Interests import Interest, UserInterest
+from sqlalchemy.orm.exc import NoResultFound
 from models import  db
 
 user_blueprint = Blueprint('user', __name__)
+
 
 @user_blueprint.route('/yolozurc', methods=['GET'] )
 def get_company():
@@ -12,10 +14,32 @@ def get_company():
     db.session.commit()
     return 'added new user!'
 
+
 @user_blueprint.route('/getyolo/<email>', methods=['GET'] )
 def get_user(email):
     user = User.query.filter_by(email=email).first()
     # interest = UserInterest.query.filter_by(user=user)
 
-
     return f'The User\'s name is: { user.get_full_name() } '
+
+
+@user_blueprint.route('/create', methods=['POST'])
+def create_user():
+    return ''
+
+
+@user_blueprint.route('/token', methods=['POST'])
+def get_token():
+    user_email = request.json.get('email')
+    user_pass = request.json.get('password')
+    try:
+        user = User.query.filter_by(email=user_email).first()
+    except NoResultFound:
+        return jsonify({'token': ''}), 401
+
+    # If password is correct, return auth token
+    if User.check_password(self=user, password=user_pass):
+        return jsonify({'token': user.generate_auth_token()}), 200
+    else:
+        return jsonify({'token': ''}), 401
+
