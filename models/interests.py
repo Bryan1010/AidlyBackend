186910603e37c78d1fd24abcdb2514904d2258c8
Derek <7885194+derekstephen@users.noreply.github.com>
2020-07-22 20:55:44 -0400

    Updated Interest Tables PK

--- models/interests.py
+++ models/interests.py
@@ -15,13 +15,14 @@ class UserInterest(db.Model):
     """ User Interest Model data class """
     __tablename__ = "UserInterests"
     id = Column(Integer, index=True, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, db.ForeignKey('Users.id'))
-    interest_id = Column(Integer, db.ForeignKey('Interests.id'))
+    user_id = Column(Integer, db.ForeignKey('Users.id'), primary_key=True)
+    interest_id = Column(Integer, db.ForeignKey('Interests.id'), primary_key=True)
 
 
 class CompanyInterest(db.Model):
     """ Company Interest Model data class """
     __tablename__ = "CompanyInterests"
     id = Column(Integer, index=True, primary_key=True, autoincrement=True)
-    company_id = Column(Integer, db.ForeignKey('Companies.id'))
-    interest_id = Column(Integer, db.ForeignKey('Interests.id'))
+    company_id = Column(Integer, db.ForeignKey('Companies.id'), primary_key=True)
+    interest_id = Column(Integer, db.ForeignKey('Interests.id'), primary_key=True)
+
